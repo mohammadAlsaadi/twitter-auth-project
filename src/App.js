02706e448d621ee0da00b362c4a82f73eb1f6991
mohammad alsaadi <moalsaadiformal@gmail.com>
2024-05-22 11:38:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import TwitterLoginButton from "./components/TwitterLoginButton";
+import TwitterCallback from "./components/TwitterCallback";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/twitter-callback" component={TwitterCallback} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/components/TwitterLoginButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { TwitterLogin } from "react-oauth/twitter";
+
+const TwitterLoginButton = () => {
+  const onSuccess = (response) => {
+    response.json().then((body) => {
+      console.log(body);
+      // Handle success - save user data, tokens, etc.
+    });
+  };
+
+  const onFailure = (error) => {
+    console.error(error);
+  };
+
+  return (
+    <TwitterLogin
+      loginUrl="http://localhost:3001/api/v1/auth/twitter"
+      onFailure={onFailure}
+      onSuccess={onSuccess}
+      requestTokenUrl="http://localhost:3001/api/v1/auth/twitter/reverse"
+      consumerKey="YOUR_CONSUMER_KEY"
+      consumerSecret="YOUR_CONSUMER_SECRET"
+    />
+  );
+};
+
+export default TwitterLoginButton;
